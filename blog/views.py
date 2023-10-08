42ffff23d80f180a2993599c5446a37aa159f447
danimlisto16 <dyanez4@outlook.com>
2023-10-08 01:58:11 -0500

    POST CRUD COMPLETED SUCCESFULLY

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
 # blog/views.py
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy
+
 from .models import Post #Class created in App/models
 
 
@@ -10,5 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = "post_detail.html"
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body","color"]
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "author", "body","color"]
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = "post_delete.html"
+    fields = ["title", "author", "body","color"]
+    success_url = reverse_lazy("home")
 
 
