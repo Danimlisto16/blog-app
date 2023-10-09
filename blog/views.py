90ac164ec7b8cb0ec887235abd6e698bc75cb640
danimlisto16 <dyanez4@outlook.com>
2023-10-08 23:09:42 -0500

    TEST CASES reapred, the problema was with color code

--- blog/views.py
+++ blog/views.py
@@ -1,10 +1,8 @@
-# blog/views.py
-from django.views.generic.edit import CreateView,UpdateView,DeleteView
 from django.views.generic import ListView, DetailView
-from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView, UpdateView, DeleteView  # new
+from django.urls import reverse_lazy  # new
 
-from .models import Post #Class created in App/models
-
+from .models import Post
 
 class BlogListView(ListView):
     model = Post
@@ -24,10 +22,7 @@ class BlogUpdateView(UpdateView):
     template_name = "post_edit.html"
     fields = ["title", "body","color"]
 
-class BlogDeleteView(DeleteView):
+class BlogDeleteView(DeleteView):  # new
     model = Post
     template_name = "post_delete.html"
     success_url = reverse_lazy("home")
-    
-
-
